a2b46f6ab510d1d57af14e234f65c907ce5ce331
Denisa Kita <denisakita7@gmail.com>
2024-05-27 09:58:36 +0200

    feat: using get and post requests

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
 @app.route('/')
-def home():  # put application's code here
+def home():
     return render_template('home.html')
 
 
-@app.route('/about')
-def about():
-    return 'About'
-
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return "This is not valid"
 
 if __name__ == '__main__':
     app.run(debug=True)
