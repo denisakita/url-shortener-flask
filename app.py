1f70fb21b84724c68d2bc3f42d1a9ced70ed72e1
denisakita <78655648+denisakita@users.noreply.github.com>
2024-05-27 11:12:19 +0200

    Feature (#1)
    * feat: add new template for home and about route
    
    * feat: using get and post requests
    
    * feat: saving to a json file
    
    * feat: alerting users with message flashing
    
    * feat: file uploads from users
    
    * feat: working with static files
    
    * feat: display custom error pages

--- app.py
+++ app.py
@@ -1,12 +1,66 @@
-from flask import Flask
+import json
+import os.path
+
+from flask import (Flask, render_template, request,
+                   flash, redirect, url_for, abort)
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.secret_key = '2637216381'
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        urls = {}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+
+        if request.form['code'] in urls.keys():
+            flash('That short name has already been taken.Please select another name.')
+            return redirect(url_for('home'))
+
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('/Users/amf/PycharmProjects/Flask/url-shortener-flask/static/user_files/' + full_name)
+            urls[request.form['code']] = {'file': full_name}
+
+        with open('urls.json', 'w') as urls_file:
+            json.dump(urls, urls_file)
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return redirect(url_for('home'))
+
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
+                else:
+                    return redirect(url_for('static', filename='user_files/' + urls[code]['file'])
+                                    )
+    return abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
