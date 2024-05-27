1aa997f297b781fb1cef5dc86be2b4b53c2f8af2
Denisa Kita <denisakita7@gmail.com>
2024-05-27 10:11:14 +0200

    feat: saving to a json file

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import json
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,9 +14,13 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {request.form['code']: {'url': request.form['url']}}
+        with open('urls.json', 'w') as urls_file:
+            json.dump(urls, urls_file)
         return render_template('your_url.html', code=request.form['code'])
     else:
-        return "This is not valid"
+        return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
